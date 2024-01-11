f1ebb6351dbbc9894a56b35b46e62c440516fa19
emon9X <shahariar.emn990@gmail.com>
2024-01-12 00:03:40 +0600

    more array concepts

--- array.js
+++ array.js
@@ -31,10 +31,52 @@
 }
 
 //how to change an array element
-
 {
     const cars = ["Saab", "Volvo", "BMW"];
     console.log(cars);
     cars[0] = "Opel";
     console.log(cars);
+}
+
+//how to convert an array to string
+{
+    const fruits = ["Banana", "Orange", "Apple", "Mango"];
+    let stringedFruits = fruits.toString();
+    console.log(stringedFruits);
+}
+
+//array properties
+{
+    const fruits = ["Banana", "Orange", "Apple", "Mango"];
+    let length = fruits.length;
+    console.log(length); //returns the number of elements
+}
+
+//how to add new element to an array
+{
+    //by push() method
+    const fruits = ["Banana", "Orange", "Apple"];
+    fruits.push("Lemon");  // Adds a new element (Lemon) to fruits
+    console.log(fruits);
+}
+{
+    //by length property
+    const fruits = ["Banana", "Orange", "Apple"];
+    fruits[fruits.length] = "Lemon";  // Adds a new element (Lemon) to fruits
+    console.log(fruits);
+}
+
+//how does holes are created in an array
+{
+    const fruits = ["Banana", "Orange", "Apple"];
+    fruits[6] = "Lemon";  // Creates undefined "holes" in fruits
+    console.log(fruits);
+}
+
+//how to identify an array
+{
+    const fruits = ["Banana", "Orange", "Apple"];
+    let type = typeof fruits;
+    console.log(type);
+    console.log(Array.isArray(fruits));
 }
